fe7db378f9ada73c973d6f9b47a506811b7320a7
Pedro André <48893259+PedroSAndre@users.noreply.github.com>
2021-05-17 18:06:17 +0000

    Update KVS-LocalServer.c

--- KVS-LocalServer.c
+++ KVS-LocalServer.c
@@ -4,6 +4,47 @@
 
 #define max_waiting_connections 10
 
+//Declaration of functions writen in the end
+int createAndBindServerSocket(int * localserver_sock, struct sockaddr_un * localserver_sock_addr);
+void acceptConnections(void *arg);
+void handleConnection(void *arg);
+
+//Global variables (to be shared across all server threads)
+int server_status = 1; //1 -> ON; 0 -> OFF 
+
+int main(void)
+{
+    int cycle_status = 1;
+    int selector = 0;
+    pthread_t acepting_thread_ptid;
+
+    remove(server_addr); //To remove later
+
+
+    if(pthread_create(&acepting_thread_ptid,NULL,(void *)&acceptConnections,NULL)<0)
+    {
+        perror("Error creating thread");
+        return -6;
+    }
+
+    printf("*****Welcome to KVS Local Server*****\n");
+    //Main control cycle
+    while(cycle_status)
+    {
+        printf("Select the desired option:\n0) Shutdown server\n1) Create a group\n2) Delete a group\n3) Show group info\n4) Show app status\n\n");
+        scanf("%d", &selector);
+        printf("Option selected: %d\n\n", selector);
+        if(selector==0)
+        {
+            server_status=0;
+            cycle_status=0;
+        }
+    }
+    pthread_join(acepting_thread_ptid,NULL);
+
+    return 0;
+}
+
 //Functions used to simplify code
 int createAndBindServerSocket(int * localserver_sock, struct sockaddr_un * localserver_sock_addr)
 {
@@ -30,38 +71,6 @@ int createAndBindServerSocket(int * localserver_sock, struct sockaddr_un * local
 
 
 //Thread functions
-void handleConnection(void *arg)
-{
-    int client_sock;
-    int answer;
-    int client_PID;
-
-    char * group_id;
-    char * secret;
-    group_id = malloc(1024*sizeof(char));
-    secret = malloc(1024*sizeof(char));
-
-    client_sock = *((int *)arg);
-
-    read(client_sock,&client_PID,sizeof(client_PID));
-    read(client_sock,group_id,(1024*sizeof(char)));
-    read(client_sock,secret,(1024*sizeof(char)));
-    printf("Client_PID: %d\n", client_PID);
-    printf("Group_ID: %s\n", group_id);
-    printf("Secret: %s\n", secret);
-
-    answer=1;
-    write(client_sock,&answer,sizeof(answer));
-    read(client_sock,&answer,sizeof(answer));
-    if(close(client_sock)<0)
-    {
-        perror("Error closing connection");
-        pthread_exit((void *)-5);
-    }
-
-    pthread_exit((void *)0);
-}
-
 void acceptConnections(void *arg)
 {
     int aux;
@@ -103,34 +112,34 @@ void acceptConnections(void *arg)
     pthread_exit((void *)0);
 }
 
-int main(void)
+void handleConnection(void *arg)
 {
-    int aux = 1;
-    int selector = 0;
-    pthread_t ptid;
+    int client_sock;
+    int answer;
+    int client_PID;
 
-    remove(server_addr); //To remove later
+    char * group_id;
+    char * secret;
+    group_id = malloc(1024*sizeof(char));
+    secret = malloc(1024*sizeof(char));
 
+    client_sock = *((int *)arg);
 
-    if(pthread_create(&ptid,NULL,(void *)&acceptConnections,NULL)<0)
+    read(client_sock,&client_PID,sizeof(client_PID));
+    read(client_sock,group_id,(1024*sizeof(char)));
+    read(client_sock,secret,(1024*sizeof(char)));
+    printf("Client_PID: %d\n", client_PID);
+    printf("Group_ID: %s\n", group_id);
+    printf("Secret: %s\n", secret);
+
+    answer=1;
+    write(client_sock,&answer,sizeof(answer));
+    read(client_sock,&answer,sizeof(answer));
+    if(close(client_sock)<0)
     {
-        perror("Error creating thread");
-        return -6;
+        perror("Error closing connection");
+        pthread_exit((void *)-5);
     }
 
-    printf("*****Welcome to KVS Local Server*****\n");
-    //Main control cycle
-    while(aux==1)
-    {
-        printf("Select the desired option:\n0) Shutdown server\n1) Create a group\n2) Delete a group\n3) Show group info\n4) Show app status\n\n");
-        scanf("%d", &selector);
-        printf("Option selected: %d\n\n\n", selector);
-        if(selector==0)
-        {
-            aux=0;
-        }
-    }
-    pthread_join(ptid,NULL);
-
-    return 0;
+    pthread_exit((void *)0);
 }
